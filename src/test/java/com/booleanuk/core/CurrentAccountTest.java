d15cad6848694805a4aa0cb0b3210174bc2f9c2d
Sebastian Persson <sebastian.persson.91@gmail.com>
2026-08-26 13:28:55 +0200

    Add test to get currentAccount name

---
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -0,0 +1,24 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class CurrentAccountTest {
+
+    CurrentAccount currenAccount;
+
+    @BeforeEach
+    public void init() {
+        this.currenAccount = new CurrentAccount("John Doe", BigDecimal.ZERO);
+    }
+
+    @Test
+    public void getName() {
+        String name = currenAccount.getName();
+
+        Assertions.assertEquals("John Doe", name);
+    }
+}
