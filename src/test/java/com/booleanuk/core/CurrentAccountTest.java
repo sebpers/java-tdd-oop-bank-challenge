36fda0cba6fbb4d2152bbe81ba01ba7d9a2609c0
Sebastian Persson <sebastian.persson.91@gmail.com>
2026-08-26 13:29:54 +0200

    Add test to get currentAccount balance

--- src/test/java/com/booleanuk/core/CurrentAccountTest.java
+++ src/test/java/com/booleanuk/core/CurrentAccountTest.java
@@ -21,4 +21,11 @@ public class CurrentAccountTest {
 
         Assertions.assertEquals("John Doe", name);
     }
+
+    @Test
+    public void getBalance() {
+        BigDecimal balance = currenAccount.getBalance();
+
+        Assertions.assertEquals(BigDecimal.valueOf(0), balance);
+    }
 }
