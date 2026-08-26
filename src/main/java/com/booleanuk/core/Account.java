1cf3c1ec41915454cf7886717da503d68144c0f8
Sebastian Persson <sebastian.persson.91@gmail.com>
2026-08-26 11:11:46 +0200

    Add code to getBalance in account

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -1,14 +1,22 @@
 package com.booleanuk.core;
 
+import java.math.BigDecimal;
+
 public abstract class Account {
 
     private String name;
+    private BigDecimal balance;
 
-    public Account(String name) {
+    public Account(String name, BigDecimal balance) {
         this.name = name;
+        this.balance = balance;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
     }
 }
