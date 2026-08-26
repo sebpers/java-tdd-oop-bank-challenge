fe9ed57ba4ee76a34d4e772b6817a2efcf77b9ae
Sebastian Persson <sebastian.persson.91@gmail.com>
2026-08-26 11:11:28 +0200

    Add test to getBalance in account

--- src/test/java/com/booleanuk/core/AccountTest.java
+++ src/test/java/com/booleanuk/core/AccountTest.java
@@ -3,15 +3,24 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class AccountTest {
 
     @Test
     public void shouldSetAndGetName_successfully() {
-        Account account = new Account("John Does savings") {};
+        Account account = new Account("John Does savings", BigDecimal.ZERO) {};
 
         String name = account.getName();
 
         Assertions.assertEquals("John Does savings", name);
     }
 
+    @Test
+    public void shouldHaveABalance_successfully() {
+        Account account = new Account("John Does savings", BigDecimal.ZERO) {};
+
+        Assertions.assertEquals("John Does savings", account.getName());
+        Assertions.assertEquals(BigDecimal.ZERO, account.getBalance());
+    }
 }
